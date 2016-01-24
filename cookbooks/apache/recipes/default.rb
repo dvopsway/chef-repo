d02adce4c9ae75d52f14734f796073bf8192380b
Padmakar Ojha <padmakarojha@gmail.com>
2016-01-24 17:05:25 +0530

    first cookbook example

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'httpd' do
+  action :install
+end
