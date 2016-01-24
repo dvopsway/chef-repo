8f59bdf5d2695eaebe0dcff9a6ff416fa253c80e
Padmakar Ojha <padmakarojha@gmail.com>
2016-01-24 17:30:19 +0530

    written first cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,7 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
 package 'httpd' do
   action :install
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  mode "0644"
 end
